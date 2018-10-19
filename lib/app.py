6a74e6978e32c9b59d905195d675e9f806a3b7b4
anita nseri <anaseri@student.unimelb.edu.au>
2018-10-19 15:42:11 +1100

    reading the survey file

--- lib/app.py
+++ lib/app.py
@@ -2,3 +2,16 @@ import csv
 
 surveryFileName = raw_input("Enter survery file name here: ")
 resultFileName = raw_input("Enter results file name here: ")
+
+#change according to the path that its placed at the time of running the code
+path = "/Users/anitanaseri/desktop/CLI_application_code_challenge/example-data/"
+
+#reading the survey file and stroing it in dictionary
+with open(path + surveryFileName + ".csv", 'rb') as csvfile:
+    csv_reader = csv.DictReader(csvfile, delimiter=',')
+    surveyDict = {name: [] for name in csv_reader.fieldnames}
+    line_count = 0
+    for row in csv_reader:
+        for name in csv_reader.fieldnames:
+            surveyDict[name].append(row[name])
+        line_count +=1
